cd2d869f8dba9a867bc0ad90409527cf0773a4fd
Antoine Smet <antoinesmetdev@gmail.com>
2025-02-07 11:50:32 +0100

    translate vocal

---
+++ Traduction/Vocale/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
+
+class Program 
+{
+    static void OutputSpeechRecognitionResult(TranslationRecognitionResult translationRecognitionResult)
+    {
+        switch (translationRecognitionResult.Reason)
+        {
+            case ResultReason.TranslatedSpeech:
+                foreach (var element in translationRecognitionResult.Translations)
+                {
+                    Console.WriteLine($"Traduction : {element.Value}");
+                }
+                break;
+            case ResultReason.NoMatch:
+                Console.WriteLine($"Error");
+                break;
+        }
+    }
+
+    async static Task Main(string[] args)
+    {
+        
+        string subscriptionKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        string region = "westeurope";
+        var speechTranslationConfig = SpeechTranslationConfig.FromSubscription(subscriptionKey, region);        
+        speechTranslationConfig.SpeechRecognitionLanguage = "fr-FR";
+        speechTranslationConfig.AddTargetLanguage("km");
+
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var translationRecognizer = new TranslationRecognizer(speechTranslationConfig, audioConfig);
+
+        Console.WriteLine("Je vous écoute ...");
+        var translationRecognitionResult = await translationRecognizer.RecognizeOnceAsync();
+        OutputSpeechRecognitionResult(translationRecognitionResult);
+    }
+}
---
+++ Traduction/Vocale/Readme.md
@@ -0,0 +1,8 @@
+
+## Explication sur la traduction vocale
+
+Dans le cadre de la traduction vocale, il est plus simple de créer un projet directement conçu pour s'interfacer avec le microphone d'un ordinateur, plutôt que de rester agnostique vis-à-vis des technologies spécifiques. C'est pourquoi vous trouverez un exemple de projet en C#.
+
+```
+ dotnet add package Microsoft.CognitiveServices.Speech
+```
